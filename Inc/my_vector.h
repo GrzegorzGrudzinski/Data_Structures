70e3eeedb0a80266856ce98dc25916373818cdd2
Grzegorz Grudzinski <grudzinskikg@gmail.com>
2025-09-05 15:40:36 -0400

    vector: add pop functions. add [] operator

--- Inc/my_vector.h
+++ Inc/my_vector.h
@@ -34,53 +34,59 @@ class vector
     int _size;
     int _capacity;
 
-    void _reserve () {
-        int tempCapacity;
-        if (_size == _capacity) {
-            if (_capacity == 0) {
-                tempCapacity = 1;
-            } else {
-                tempCapacity = 2*_capacity;
-            }
-
-            T* temp = new T [tempCapacity];
-            for (int i=0; i<_size; ++i) {
-                temp[i] = arr[i];
-            }
-            delete [] arr;
-            arr = temp;
-            _capacity = tempCapacity;
-        }
-    }
-
+    void _reserve ();
 public:
-
     vector();     //
+    vector(std::initializer_list<T> init);
     ~vector();    // make sure that every node is deleted
+
+    T& operator[](int index);
 
     // _vector_iterator<T> begin() {}
     // _vector_iterator<T> end() {}
 
-    void push_back(const T& data);// push node at the end
-    void push_front(const T& data); // push node to the front
-    void insert(int position, T& data); //
+    void push_back(const T& data);// push data at the end
+    void push_front(const T& data); // push data to the front
+    void insert(int position, const T& data); // insert data after the given position
+
+    // void erase(_vector_iterator<T> iter1,  _vector_iterator<T> iter2 = nullptr); //
+    void clean();
 
     void pop_back(); // delete last node
     void pop_front();// delete first node
     void pop_element(int position); // delete node at the given position
-    // void erase(_vector_iterator<T> iter1,  _vector_iterator<T> iter2 = nullptr); //
-    void clean();
-
-    T& first();
-    T& last();
-    T& get_element(int position); // return data from the given position
-
     // resize
     // reserve
 
     int get_size(); // return the size of the array
     int get_capacity(); // return the capacity of the array
 };
+
+/************************************
+ *
+ ************************************/
+
+template <typename T>
+void vector<T>::_reserve()
+{
+    int tempCapacity;
+    if (_size == _capacity) {
+        if (_capacity == 0) {
+            tempCapacity = 1;
+        } else {
+            tempCapacity = 2*_capacity;
+        }
+
+        T* temp = new T [tempCapacity];
+        for (int i=0; i<_size; ++i) {
+            temp[i] = arr[i];
+        }
+        delete [] arr;
+        arr = temp;
+        _capacity = tempCapacity;
+    }
+}
+
 
 template <typename T>
 vector<T>::vector() : arr(nullptr), _size(0), _capacity(0)
@@ -92,6 +98,23 @@ vector<T>::~vector()
     clean();
 }
 
+template<typename T>
+vector<T>::vector(std::initializer_list<T> init) : arr(nullptr), _size(0), _capacity(0)
+{
+    // for (const T& data : init) {
+        // push_back(data);
+    // }
+}
+
+
+template <typename T>
+T& vector<T>::operator[] (int index)
+{
+    if (index < _size && index >= 0) {
+        return arr[index];
+    }
+    throw std::out_of_range("Out of range");
+}
 
 template <typename T>
 void vector<T>::push_back(const T& data)
@@ -115,14 +138,14 @@ void vector<T>::push_front(const T& data)
 }
 
 template <typename T>
-void vector<T>::insert(int position, T& data)
+void vector<T>::insert(int position,const T& data)
 {
     if (position == 0) {
-        push_front();
+        push_front(data);
         return;
     }
     if (position == _size) {
-        push_back();
+        push_back(data);
         return;
     }
     if (position > _size || position < 0) {
@@ -141,11 +164,37 @@ void vector<T>::insert(int position, T& data)
 }
 
 template <typename T>
+void vector<T>::pop_back() {
+    _size--;
+}
+
+template <typename T>
+void vector<T>::pop_front() {
+    for (int i=0; i<_size-1; ++i) {
+        arr[i] = arr[i+1];
+    }
+
+    _size--;
+}
+
+
+template <typename T>
 void vector<T>::clean() {
     delete [] arr;
     arr = nullptr;
     _capacity = 0;
     _size = 0;
 }
+
+template <typename T>
+int vector<T>::get_size() {
+    return _size;
+}
+
+template <typename T>
+int vector<T>::get_capacity() {
+    return _capacity;
+}
+
 
 #endif // VECTOR_H
